e2977eaa95cfb4500a0718298814bf6e326ab52c
Jacarn <117977673+Jacarn@users.noreply.github.com>
2022-12-01 00:56:00 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+// let about1 = document.querySelector(".about1") 
+
+// function showhide1(){
+//     about1.style.display = "flex";
+// }
+
+function showhide1() {
+    var x = document.getElementById("about1") ("about2");
+    if (about1.style.display === "none") {
+        about1.style.display = "block";
+        about2.style.display = "none";
+    } 
+  }
